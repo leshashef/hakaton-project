2395f7dd395affbcbdb8b7e9b6d4a38ef3a4be17
Alexey Shevchenko <lesha-shevchenko2001@mail.ru>
2022-04-01 23:04:50 +0300

    up

---
+++ src/views/ListTestPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            listTest:[]
+        }
+    },
+    created(){
+        this.getList();
+    },
+    methods:{
+        async getList(){
+            let response = await fetch("https://localhost:7101/api/ListTest",
+            {
+                method: 'GET',
+                mode: 'cors',
+                credentials: 'include'
+            }
+            );
+
+            if (response.ok) { // если HTTP-статус в диапазоне 200-299
+            // получаем тело ответа (см. про этот метод ниже)
+            let json = await response.json();
+            console.log(json);
+            } else {
+            alert("Ошибка HTTP: " + response.status);
+            }
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
