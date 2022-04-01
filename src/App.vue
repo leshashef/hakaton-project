ea37ff06e90d39a10f7b5a7e240356a3e20df986
Gleb <aslanovgleb@mail.ru>
2022-04-01 17:42:04 +0300

    new files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+  </header>
+
+  <footer>
+     <router-link to = '/about'> About Page </router-link>
+      <router-link to = '/'> Home Page </router-link>
+  </footer>
+  <router-view/>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
+  
   }
 }
 </script>
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "@/views/HomePage.vue";
+import About from "@/views/AboutPage.vue";
+import NotFound from "@/views/NotFoundPage.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "HomePage",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "AboutPage",
+    component: About,
+  },
+
+    {
+      path: "/:catchAll(.*)",
+      component: NotFound,
+    },
+  
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
