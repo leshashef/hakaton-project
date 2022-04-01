0bb794bb488350cc094836d4317a435251eb769e
Nikita <bleakwhite@mail.ru>
2022-04-01 20:13:40 +0300

    up

--- src/views/WelcomePages.vue
+++ src/views/WelcomePages.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="background-img">
-    <div>
+    <div class="text">
+       <div class="tittle">
+           Добро пожаловать
+      
+        </div> 
+        <div class="Titlesecond">
+           Данное приложение позволит вам определить вашу<br> предрасположенность в it
 
+        </div>
+    </div> 
+    <div class="tittle">
+    
+      
+      
     </div> 
   </div>
+  
    
 </template>
 
