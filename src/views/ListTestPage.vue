754fbd9fff725444fe84091e9ec4318767a5f55a
Gleb <aslanovgleb@mail.ru>
2022-04-02 22:49:53 +0300

    up

--- src/views/ListTestPage.vue
+++ src/views/ListTestPage.vue
@@ -3,9 +3,29 @@
 
     <div class="list-tests-background-left-side">
 
+        <div class="registration-button-menu">
+            <div class="registration-button-menu-part1">
+            </div>
+            <div class="registration-button-menu-part2">
+            </div>
+             <div class="registration-button-menu-part3">
+            </div>
+        </div>
+
+        <div class="list-tests-title-left-side">
+            IT-Trend
+        </div>
+
       <div class="list-tests-title-background-left-side"> 
        Название теста
       </div>
+
+      <div class="list-tests-choise-test-title-left-side">
+        Выберите один из предложенных тестов
+      </div>
+      
+
+
 
     </div>
 
@@ -13,12 +33,34 @@
 
     </div>
 
-    </div>
+  
+        
+        </div>
+
+          <div class="list-tests-footer">
+
+            <div class="list-tests-test">
+         <img class="list-tests-img-test" ref="/img/img/Clogo.png">
+         <div class="list-tests-title-test">
+             Название теста
+         </div>
+             <div class="list-test-sign-who-made-test">
+               
+             </div>
+              <div class="list-test-title-who-made-test">
+             
+                     Создал:
+                 </div>
+            </div>
+      </div>
+
     
 </template>
 
 <script>
+import AboutPage from './AboutPage.vue';
 export default {
+  components: { AboutPage },
     data(){
         return{
             listTest:[]
